a9987ae6f587a4d010859630256ba6c07c8516fa
fshangala <fshangala@gmail.com>
2025-02-24 09:19:58 +0200

    refactor: simplify Index class and streamline page rendering

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 include('page.php');
 
-class Index extends Page {
-  public function __construct() {
-    $this->title = 'Mayo';
-    $this->content = 'Welcome to the home page.';
-  }
-}
-
-$index = new Index();
+$index = new Page();
 $index->render();
--- page.php
+++ page.php
@@ -3,25 +3,6 @@ class Page {
   public $title = 'Default';
   public $content = 'This is the default content.';
 
-  function renderHead() {
-    include('head.php');
-  }
-
-  function renderHeader() {
-    echo '<header>';
-    echo '<h1>' . $this->title . '</h1>';
-    echo '</header>';
-  }
-
-  function renderContent() {
-    echo '<p>' . $this->content . '</p>';
-  }
-
-  function renderFooter() {
-    echo '</body>';
-    echo '</html>';
-  }
-
   function render() {
     ?>
 
@@ -30,18 +11,17 @@ class Page {
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
     <?php
-    $this->renderHead();
+    include('head.php');
     ?>
  
     <body class="home page-template-default page page-id-15 wp-embed-responsive ui-a-dsmm-slide  elementor-default elementor-kit-3 elementor-page elementor-page-15">
-    
+    <div class="uicore-animation-bg"></div>
+    <div class="uicore-body-content">
     <?php
-    $this->renderHeader();
-    $this->renderContent();
-    $this->renderFooter();
+    include('footer.php');
     include('trailing.php');
     ?>
-
+    </div>
     </body>
     </html>
 
