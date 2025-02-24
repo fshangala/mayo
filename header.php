a6178a433799465d8d2e678cdd7c54c70a01726c
fshangala <fshangala@gmail.com>
2025-02-24 09:51:21 +0200

    feat: add header and update page structure for improved navigation

---
+++ header.php
@@ -0,0 +1,62 @@
+<div data-uils="header" data-uils-title="Header" id="wrapper-navbar" itemscope
+  itemtype="http://schema.org/WebSite"
+  class="uicore uicore-navbar elementor-section elementor-section-boxed uicore-h-classic uicore-sticky ui-smart-sticky uicore-transparent ">
+  <div class="uicore-header-wrapper">
+    <nav class="uicore elementor-container">
+      <div class="uicore-branding " data-uils="header-branding" data-uils-title="Site Logo">
+
+        <a href="index.html" rel="home">
+          <img class="uicore uicore-logo uicore-main" src="wp-content/uploads/sites/7/2024/06/Upshift-Logo.webp"
+            alt="Web App" />
+          <img class="uicore uicore-logo uicore-second" src="wp-content/uploads/sites/7/2024/06/Upshift-Logo.webp"
+            alt="Web App" />
+          <img class="uicore uicore-logo uicore-mobile-main"
+            src="wp-content/uploads/sites/7/2024/06/Upshift-Logo.webp" alt="Web App" />
+          <img class="uicore uicore-logo uicore-mobile-second"
+            src="wp-content/uploads/sites/7/2024/06/Upshift-Logo.webp" alt="Web App" />
+        </a>
+
+      </div>
+      <div class='uicore-nav-menu'>
+        <div class="uicore-menu-container uicore-nav">
+          <ul data-uils="header-menu" data-uils-title="Navigation Menu" class="uicore-menu">
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-147"><a
+                href="#about"><span class="ui-menu-item-wrapper">About</span></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-10"><a
+                href="#benefits"><span class="ui-menu-item-wrapper">Benefits</span></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-11"><a
+                href="#integrations"><span class="ui-menu-item-wrapper">Integrations</span></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-12"><a
+                href="#pricing"><span class="ui-menu-item-wrapper">Pricing</span></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13"><a
+                href="#testimonials"><span class="ui-menu-item-wrapper">Testimonials</span></a></li>
+          </ul>
+        </div>
+        <div class="uicore uicore-extra" data-uils="header_extra" data-uils-title="Header Extras">
+          <div class="uicore-cta-wrapper">
+            <a href="#" target="_self" class="uicore-btn ">
+              <span class="elementor-button-text">
+                Sign In </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="uicore-mobile-head-right">
+        <div class="uicore-cta-wrapper">
+          <a href="#" target="_self" class="uicore-btn ">
+            <span class="elementor-button-text">
+              Sign In </span>
+          </a>
+        </div>
+        <button type="button" class="uicore-toggle uicore-ham" aria-label="mobile-menu">
+          <span class="bars">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+          </span>
+        </button>
+      </div>
+    </nav>
+
+  </div>
+</div><!-- #wrapper-navbar end -->
---
+++ hero.php
@@ -0,0 +1,143 @@
+<div class="elementor-element elementor-element-d753b1d e-flex e-con-boxed e-con e-parent"
+  data-id="d753b1d" data-element_type="container"
+  data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+  <div class="e-con-inner">
+    <div class="elementor-element elementor-element-8b12462 e-con-full e-flex e-con e-child"
+      data-id="8b12462" data-element_type="container"
+      data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+      <div
+        class="elementor-element elementor-element-82fe876 ui-borderanim-rotate ui-gradient elementor-invisible elementor-widget elementor-widget-heading"
+        data-id="82fe876" data-element_type="widget"
+        data-settings="{&quot;_animation_delay&quot;:300,&quot;_animation&quot;:&quot;fadeInUp&quot;}"
+        data-widget_type="heading.default">
+        <div class="elementor-widget-container">
+          <h6 class="elementor-heading-title elementor-size-default">Meet Mayo</h6>
+        </div>
+      </div>
+      <div
+        class="elementor-element elementor-element-e1fa6a3 elementor-widget__width-initial elementor-widget-mobile__width-inherit elementor-widget-tablet__width-initial elementor-invisible elementor-widget elementor-widget-heading"
+        data-id="e1fa6a3" data-element_type="widget"
+        data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:360}"
+        data-widget_type="heading.default">
+        <div class="elementor-widget-container">
+          <h1 class="elementor-heading-title elementor-size-default">Transform your HR and payroll
+            processes today</h1>
+        </div>
+      </div>
+      <div
+        class="elementor-element elementor-element-9412fd0 elementor-widget__width-initial elementor-widget-mobile__width-inherit elementor-widget-tablet__width-initial elementor-invisible elementor-widget elementor-widget-text-editor"
+        data-id="9412fd0" data-element_type="widget"
+        data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:420}"
+        data-widget_type="text-editor.default">
+        <div class="elementor-widget-container">
+          Streamline operations, ensure compliance, and enhance employee satisfaction with our
+          all-in-one HR and payroll solution. </div>
+      </div>
+      <div
+        class="elementor-element elementor-element-97b8041 elementor-align-center elementor-invisible elementor-widget elementor-widget-button"
+        data-id="97b8041" data-element_type="widget"
+        data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:480}"
+        data-widget_type="button.default">
+        <div class="elementor-widget-container">
+          <div class="elementor-button-wrapper">
+            <a class="elementor-button elementor-button-link elementor-size-sm" href="#">
+              <span class="elementor-button-content-wrapper">
+                <span class="elementor-button-icon">
+                  <svg aria-hidden="true" class="e-font-icon-svg e-fas-angle-right"
+                    viewBox="0 0 256 512" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z">
+                    </path>
+                  </svg> </span>
+                <span class="elementor-button-text">Get Started</span>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        class="elementor-element elementor-element-49af331 elementor-invisible elementor-widget elementor-widget-text-editor"
+        data-id="49af331" data-element_type="widget"
+        data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:540}"
+        data-widget_type="text-editor.default">
+        <div class="elementor-widget-container">
+          Used by leading companies around the world </div>
+      </div>
+      <div
+        class="elementor-element elementor-element-6d400f3 elementor-invisible elementor-widget elementor-widget-uicore-logo-grid"
+        data-id="6d400f3" data-element_type="widget"
+        data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:600,&quot;columns&quot;:&quot;4&quot;,&quot;layout&quot;:&quot;inner-border&quot;}"
+        data-widget_type="uicore-logo-grid.default">
+        <div class="elementor-widget-container">
+          <div class='ui-e-grid'>
+            <div class="ui-e-wrp">
+              <div class='ui-e-animations-wrp  ui-e-item-anim-fade'>
+                <div class="ui-e-item">
+                  <figure class="ui-e-item-anim-zoom">
+                    <img fetchpriority="high" decoding="async" width="378" height="148"
+                      src="wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-1.webp"
+                      class="attachment-full size-full" alt=" : "
+                      srcset="https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-1.webp 378w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-1-300x117.webp 300w"
+                      sizes="(max-width: 378px) 100vw, 378px" />
+                  </figure>
+                </div>
+              </div>
+            </div>
+            <div class="ui-e-wrp">
+              <div class='ui-e-animations-wrp  ui-e-item-anim-fade'>
+                <div class="ui-e-item">
+                  <figure class="ui-e-item-anim-zoom">
+                    <img decoding="async" width="378" height="148"
+                      src="wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-2.webp"
+                      class="attachment-full size-full" alt=" : "
+                      srcset="https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-2.webp 378w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-2-300x117.webp 300w"
+                      sizes="(max-width: 378px) 100vw, 378px" />
+                  </figure>
+                </div>
+              </div>
+            </div>
+            <div class="ui-e-wrp">
+              <div class='ui-e-animations-wrp  ui-e-item-anim-fade'>
+                <div class="ui-e-item">
+                  <figure class="ui-e-item-anim-zoom">
+                    <img decoding="async" width="378" height="148"
+                      src="wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-3.webp"
+                      class="attachment-full size-full" alt=" : "
+                      srcset="https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-3.webp 378w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-3-300x117.webp 300w"
+                      sizes="(max-width: 378px) 100vw, 378px" />
+                  </figure>
+                </div>
+              </div>
+            </div>
+            <div class="ui-e-wrp">
+              <div class='ui-e-animations-wrp  ui-e-item-anim-fade'>
+                <div class="ui-e-item">
+                  <figure class="ui-e-item-anim-zoom">
+                    <img loading="lazy" decoding="async" width="378" height="148"
+                      src="wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-4.webp"
+                      class="attachment-full size-full" alt=" : "
+                      srcset="https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-4.webp 378w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Companies-Image-4-300x117.webp 300w"
+                      sizes="(max-width: 378px) 100vw, 378px" />
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="elementor-element elementor-element-027d5ff elementor-invisible bdt-background-overlay-yes elementor-widget elementor-widget-image"
+      data-id="027d5ff" data-element_type="widget"
+      data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:&quot;900&quot;}"
+      data-widget_type="image.default">
+      <div class="elementor-widget-container">
+        <img loading="lazy" decoding="async" width="1764" height="1103"
+          src="wp-content/uploads/sites/7/2024/06/Web-App-Hero-Image.webp"
+          class="attachment-full size-full wp-image-68" alt=""
+          srcset="https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Hero-Image.webp 1764w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Hero-Image-300x188.webp 300w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Hero-Image-1024x640.webp 1024w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Hero-Image-768x480.webp 768w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Hero-Image-1536x960.webp 1536w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Hero-Image-650x406.webp 650w"
+          sizes="(max-width: 1764px) 100vw, 1764px" />
+      </div>
+    </div>
+  </div>
+</div>
