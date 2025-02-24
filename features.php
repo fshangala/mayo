7508bcdabe37e3db7aed2d3473fe18c3c8f3ea80
fshangala <fshangala@gmail.com>
2025-02-24 12:15:16 +0200

    feat: add new content sections for about, benefits, features, integrations, and FAQ

---
+++ features.php
@@ -0,0 +1,73 @@
+<div class="elementor-element elementor-element-b962e50 e-flex e-con-boxed e-con e-parent"
+  data-id="b962e50" data-element_type="container">
+  <div class="e-con-inner">
+    <div class="elementor-element elementor-element-9ef058c e-con-full e-flex e-con e-child"
+      data-id="9ef058c" data-element_type="container"
+      data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+      <div
+        class="elementor-element elementor-element-9ca6897 elementor-invisible elementor-widget elementor-widget-image"
+        data-id="9ca6897" data-element_type="widget"
+        data-settings="{&quot;_animation&quot;:&quot;fadeInDown&quot;,&quot;_animation_delay&quot;:&quot;160&quot;}"
+        data-widget_type="image.default">
+        <div class="elementor-widget-container">
+          <img loading="lazy" decoding="async" width="784" height="160"
+            src="wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-1.webp"
+            class="attachment-full size-full wp-image-82" alt=""
+            srcset="https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-1.webp 784w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-1-300x61.webp 300w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-1-768x157.webp 768w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-1-650x133.webp 650w"
+            sizes="(max-width: 784px) 100vw, 784px" />
+        </div>
+      </div>
+      <div
+        class="elementor-element elementor-element-f265d2a elementor-invisible elementor-widget elementor-widget-image"
+        data-id="f265d2a" data-element_type="widget"
+        data-settings="{&quot;_animation&quot;:&quot;fadeInDown&quot;,&quot;_animation_delay&quot;:&quot;240&quot;}"
+        data-widget_type="image.default">
+        <div class="elementor-widget-container">
+          <img loading="lazy" decoding="async" width="784" height="702"
+            src="wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-2.webp"
+            class="attachment-full size-full wp-image-83" alt=""
+            srcset="https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-2.webp 784w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-2-300x269.webp 300w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-2-768x688.webp 768w, https://upshift.uicore.co/web-app/wp-content/uploads/sites/7/2024/06/Web-App-Automated-Image-2-650x582.webp 650w"
+            sizes="(max-width: 784px) 100vw, 784px" />
+        </div>
+      </div>
+    </div>
+    <div class="elementor-element elementor-element-9438406 e-con-full e-flex e-con e-child"
+      data-id="9438406" data-element_type="container">
+      <div
+        class="elementor-element elementor-element-47e7cf4 elementor-widget__width-initial elementor-widget-tablet__width-initial elementor-widget-mobile__width-inherit elementor-widget elementor-widget-heading"
+        data-id="47e7cf4" data-element_type="widget" data-widget_type="heading.default">
+        <div class="elementor-widget-container">
+          <h2 class="elementor-heading-title elementor-size-default">Automated payroll system</h2>
+        </div>
+      </div>
+      <div
+        class="elementor-element elementor-element-a7e705e elementor-widget elementor-widget-text-editor"
+        data-id="a7e705e" data-element_type="widget" data-widget_type="text-editor.default">
+        <div class="elementor-widget-container">
+          Eliminate errors and reduce administrative burdens with our automated payroll system that
+          ensures accurate and on-time payments. Includes features like tax calculation, benefits
+          deduction, and direct deposit. </div>
+      </div>
+      <div
+        class="elementor-element elementor-element-89a23bb elementor-mobile-align-center elementor-widget elementor-widget-button"
+        data-id="89a23bb" data-element_type="widget" data-widget_type="button.default">
+        <div class="elementor-widget-container">
+          <div class="elementor-button-wrapper">
+            <a class="elementor-button elementor-button-link elementor-size-sm" href="#">
+              <span class="elementor-button-content-wrapper">
+                <span class="elementor-button-icon">
+                  <svg aria-hidden="true" class="e-font-icon-svg e-fas-angle-right"
+                    viewBox="0 0 256 512" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z">
+                    </path>
+                  </svg> </span>
+                <span class="elementor-button-text">Learn More</span>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
